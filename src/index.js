89dcb86dff7eedf42a11e1a8b6305b9a37151adb
Sergeyy321 <125265794+Sergeyy321@users.noreply.github.com>
2023-07-29 10:20:21 +0200

    fix 20

--- src/index.js
+++ src/index.js
@@ -4,60 +4,43 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
-const totalHits = img.totalHits;
-const allImages = pixabayApi.hasMorePhotos();
-    
+// const totalHits = img.totalHits;
+// const allImages = pixabayApi.hasMorePhotos();
 const refs = {
   searchForm: document.querySelector('#search-form'),
   searchInput: document.querySelector('input[name="searchQuery"]'),
   galleryContainer: document.querySelector('.gallery-list'),
 };
-
 const imageApiService = new ImageAPiService();
 const loadMoreBtn = new LoadMoreBtn({ selector: '.load-more', hidden: true });
 const imageLightbox = new SimpleLightbox('.gallery-list .gallery__link', {
   captionsData: 'alt',
   captionDelay: 250,
 });
-
-
 refs.searchForm.addEventListener('submit', handleSearchImageClickBtn);
 loadMoreBtn.refs.button.addEventListener('click', handleLoadMoreImage);
-
 async function handleSearchImageClickBtn(e) {
   e.preventDefault();
-  console.log(e);
+  console.log(e); //це можна усунути
   refs.searchInput.focus();
-
   imageApiService.query = e.target.searchQuery.value.trim();
   imageApiService.resetPage();
-
   if (imageApiService.query === '') {
     Notify.info('Please fill in the field to search for images');
     loadMoreBtn.hide();
     clearMarkup();
     return;
   }
-
   loadMoreBtn.show();
   loadMoreBtn.disable();
   clearMarkup();
-
   try {
+    loadMoreBtn.enable();
     const response = await imageApiService.fetchImages();
-
-    if (response.hits.length === 0) {
+    if (response.totalHits === 0) {
+      //не потрібно провіряти довжину масиву, у нас є totalHits у кожному виклику цієї функції
       Notify.failure(
         'Sorry, there are no images matching your search query. Please try again'
-      );
-      loadMoreBtn.hide();
-      return;
-    } else if (response.hits.length < 16) {
-      Notify.success(`Hooray! We found ${response.totalHits} images.`);
-      refs.galleryContainer.insertAdjacentHTML(
-        'beforeend',
-        appendGalleryMarkup(response)
       );
       loadMoreBtn.hide();
       return;
@@ -67,52 +50,42 @@ async function handleSearchImageClickBtn(e) {
       'beforeend',
       appendGalleryMarkup(response)
     );
-
     imageLightbox.refresh();
-    smoothScroll(1.2);
-    loadMoreBtn.enable();
+    smoothScroll(1.2); //тут ця функція не потрібна, ти при першому виклику функції одразу проскролюєш вниз, її треба використовувати тільки коли натискаєш кнопку ЛоадМоре
+    if (response.totalHits <= 40) {
+      loadMoreBtn.hide();
+    }
   } catch (error) {
     Notify.warning('We are sorry. There was an error');
   }
 }
-
 async function handleLoadMoreImage() {
-  imageApiService.incrementPage();
-  loadMoreBtn.disable();
   try {
+    imageApiService.incrementPage();
+    loadMoreBtn.disable();
     const response = await imageApiService.fetchImages();
-if ( (totalHits - allImages) < per_Page) {
-    div.innerHTML += createMarkup(images);
-    lightbox.refresh();
-    Notify.info("We're sorry, but you've reached the end of search results.");
-    btnLoadMore.style.visibility = `hidden`;
-    return;
-    }
-    if (
-      Math.ceil(response.totalHits / imageApiService.count) ===
-      imageApiService.page
-    ) {
-      Notify.failure(
-        "We're sorry, but you've reached the end of search results"
-      );
-      loadMoreBtn.hide();
-    }  if (response.hits.length < 41) {
-      loadMoreBtn.hide();
-    }
+    loadMoreBtn.enable();
     Notify.success(`Hooray! We found more images.`);
     refs.galleryContainer.insertAdjacentHTML(
       'beforeend',
       appendGalleryMarkup(response)
     );
-   
     imageLightbox.refresh();
+    pageNumbers(response);
     smoothScroll(2);
-    loadMoreBtn.enable();
   } catch (error) {
     Notify.warning('We are sorry. There was an error');
   }
 }
-
+// функція перевірки кількості зображень для сторінок. Якщо у нас на новій сторінці менше 40 елементів, то ми прибираємо кнопу
+function pageNumbers(response) {
+  if (Math.ceil(response.totalHits / 40) !== imageApiService.page) {
+    return;
+  }
+  console.log('aaaaaaa');
+  loadMoreBtn.hide(); //постарайся наступного разу не називату зміну так само як класс тільки з маленької літери, це потім призводить до багатьох помилок
+  Notify.failure("We're sorry, but you've reached the end of search results");
+}
 function appendGalleryMarkup({ hits }) {
   const markUp = hits.map(
     ({
@@ -170,27 +143,22 @@ function appendGalleryMarkup({ hits }) {
   );
   return markUp.join('');
 }
-
 function clearMarkup() {
   refs.galleryContainer.innerHTML = '';
   refs.searchInput.value = '';
 }
-
 function smoothScroll(num) {
   const { height: cardHeight } = document
     .querySelector('.gallery-list')
     .firstElementChild.getBoundingClientRect();
-
   window.scrollBy({
     top: cardHeight * num,
     behavior: 'smooth',
   });
 }
-
 window.onscroll = debounce(() => {
   scrollFunction();
 }, 300);
-
 function scrollFunction() {
   if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
     document.getElementById('topBtn').style.display = 'block';
@@ -198,12 +166,9 @@ function scrollFunction() {
     document.getElementById('topBtn').style.display = 'none';
   }
 }
-
 const topBtn = document.querySelector('.top-btn');
 topBtn.addEventListener('click', topFunction);
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-
-
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,16 +1,14 @@
 import axios from 'axios';
-
 const API_URL = 'https://pixabay.com/api/';
 const API_KEY = '33447079-0ba3d1fd30cda0252aa7b7ada';
 const perPage = 40;
-let page = 1;
+let page = 1; //ця змінна нічого не робить, у тебе в класі прописана змінна, котра далі використовується в коді
 export default class imagesAPIService {
   constructor() {
     this.searchQuery = '';
     this.page = 1;
-    this.count = 16;
+    this.count = 40;
   }
-
   async fetchImages() {
     try {
       const response = await axios.get(`${API_URL}`, {
@@ -19,7 +17,7 @@ export default class imagesAPIService {
           q: this.searchQuery,
           image_type: 'photo',
           orientation: 'horizontal',
-          page: page,
+          page: this.page, //+this тут необхідно додати this. бо ти працюєш зі змінною класу
           per_page: perPage,
           safesearch: true,
         },
@@ -29,21 +27,16 @@ export default class imagesAPIService {
       console.log(error);
     }
   }
-
   get query() {
     return this.searchQuery;
   }
-
   set query(newQuery) {
     this.searchQuery = newQuery;
   }
-
   incrementPage() {
     this.page += 1;
   }
-
   resetPage() {
     this.page = 1;
   }
 }
-
